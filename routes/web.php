cd65c6e01718aa2d8fc7f98681dc844171371e64
--global <mathijsvandenbroeck@gmail.com>
2024-02-02 17:50:33 +0100

    feat: add basic Inertia/Svelte implementation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function() {
+    return Inertia::render('Welcome', [
+        'title' => 'Hello Laravel Svelte!',
+    ]);
+})->name('home');
